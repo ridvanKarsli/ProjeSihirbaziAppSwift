d055cc1e3611a2569a9cbeef2f665ca4ea246abf
ridvanKarsli <148614751+ridvanKarsli@users.noreply.github.com>
2025-01-04 18:40:18 +0300

    Initial Commit

---
+++ ProjeSihirbaziSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProjeSihirbaziSwiftUI
+//
+//  Created by Rıdvan Karslı on 4.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
